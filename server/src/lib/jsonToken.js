003912872f6567fe0188a260443d6df07b608e3b
Ashwani Yadav <yadav8800766165@gmail.com>
2025-07-12 19:07:56 +0530

    hello

--- server/src/lib/jsonToken.js
+++ server/src/lib/jsonToken.js
@@ -8,7 +8,7 @@ const generateToken=(userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:5*24*60*60*1000,
         httpOnly:true,
-        secure:process.env.NODE_ENV !=='development',
+        secure:true,
         sameSite:"lax"
     })
     return token;
