b2c82cce2ac63e0bcc9f8ec0ccdc3415d8844808
Ashwani Yadav <yadav8800766165@gmail.com>
2025-08-21 13:26:24 +0530

    today solve the issue of logi2n

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use('/api/auth',authRoute)
 app.use('/api/image',imageRouter)
 
 app.get("/", (req, res) => {
-  res.json({ success: true, message: "Backend running on Vercel 🚀" });
+  res.send("heloo vercel");
 });
 
 
